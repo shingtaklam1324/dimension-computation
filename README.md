6915599bc31be195a831a0dfbe17bafdb7648931
Shing Tak Lam <shingtaklam1324@gmail.com>
2025-04-13 20:30:19 +0100

    update

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Some code to compute the dimension of possible values of $\mathrm{Hom}_G(V, V)$ 
 Schur's lemma tells us that $\mathrm{Hom}_G(V, W)$ is $0$-dimensional if $V$ and $W$ are not isomorphic, and $1$-dimensional if they are. The question then becomes a purely combinatorial one, which this code computes.
 
 As a question in representation theory, the dimension can be computed as an inner product $\langle \chi_V, \chi_V \rangle$ of characters. What this code does is compute the possible values of the dimension, rather than for any specific representation/character.
+
+The total number is [A006171](https://oeis.org/A006171) and the dimensions that appear (without multiplicity) are [A215905](https://oeis.org/A215905).
--- src/Lib.hs
+++ src/Lib.hs
@@ -3,6 +3,7 @@ module Lib (displayIrrepsPartitions) where
 
 import Control.Monad (forM_)
 import Math.Combinat.Partitions.Integer (partitions, toExponentialForm, fromPartition)
+import Data.List (sortOn)
 
 
 partitionLists :: Int -> Int -> [[(Int, Int)]]
@@ -25,7 +26,7 @@ dimension :: [(Int, Int)] -> Int
 dimension = sum . map (\(_, b) -> b * b)
 
 irrepsPartitionsWithDimensions :: Int -> [([(Int, Int)], Int)]
-irrepsPartitionsWithDimensions = map (\l -> (l, dimension l)) . irrepsPartitions
+irrepsPartitionsWithDimensions = sortOn snd . map (\l -> (l, dimension l)) . irrepsPartitions
 
 log10 :: Int -> Int
 log10 x | x < 10 = 1
@@ -36,3 +37,4 @@ displayIrrepsPartitions n = do
   let irreps = irrepsPartitionsWithDimensions n
   forM_ irreps $ \(l, d) -> do
     putStrLn $ show d ++ replicate (12 - log10 d) ' ' ++ showPairs l
+  putStrLn $ "Total number of irreps: " ++ show (length irreps)
